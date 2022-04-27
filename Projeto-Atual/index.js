aca4a42f2fbd928839c8bee4e597ad3232f4f345
Renan Lemos <renanlemos1612@gmail.com>
2022-04-27 15:38:06 -0400

    Update

--- Projeto-Atual/index.js
+++ Projeto-Atual/index.js
@@ -1,26 +1,46 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
-    rg;
 }
 // a classe (class Cliente) é o molde. Os clientes são o Objeto.
 // Ctrl + Alt você edita várias linhas de uma única vez.
+
+class ContaCorrente {
+    agencia;
+    saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+}
+let valorSacado = 200;
+
 
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
 cliente1.nome = "Renan";
 cliente1.cpf = "11122233309";
-cliente1.agencia = "1001";
-cliente1.saldo = 0;
-cliente1.rg = "1.222-111";
 
-cliente2.nome = "Alice";
+cliente2.nome = "Kelly";
 cliente2.cpf = "88822233309";
-cliente2.agencia = "1001";
-cliente2.saldo = 0;
+
+const contaCorrenteRenan = new ContaCorrente();
+
+contaCorrenteRenan.saldo = 0;
+contaCorrenteRenan.agencia = 1001;
 
 
-console.log(cliente1, cliente2);
+console.log(contaCorrenteRenan.saldo);
+contaCorrenteRenan.saldo = 100;
+console.log(contaCorrenteRenan.saldo);
+contaCorrenteRenan.sacar(200);
+
+
+
+
+console.log(contaCorrenteRenan.saldo);
+console.log(cliente1);
+console.log(cliente2);
