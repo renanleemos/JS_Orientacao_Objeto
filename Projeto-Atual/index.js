862bb01d612c41452f7737743cf1e051cc7142c7
Renan Lemos <renanlemos1612@gmail.com>
2022-05-13 17:53:01 -0400

    Update

--- Projeto-Atual/index.js
+++ Projeto-Atual/index.js
@@ -4,28 +4,18 @@ import {ContaCorrente} from "./ContaCorrente.js";
 // a classe (class Cliente) é o molde. Os clientes são o Objeto.
 // Ctrl + Alt você edita várias linhas de uma única vez.
 
-
-
-const cliente1 = new Cliente();
-cliente1.nome = "Renan";
-cliente1.cpf = "11122233309";
-
 //const cliente2 = new Cliente();
 //cliente2.nome = "Kelly";
 //cliente2.cpf = "88822233309";
 
+const cliente1 = new Cliente("Renan", 11122233309);
+const cliente2 = new Cliente("Kelly", 88822233309);
+console.log(cliente2.cpf);
 
-const contaCorrenteRenan = new ContaCorrente();
-contaCorrenteRenan.agencia = 1001;
-contaCorrenteRenan.cliente =  cliente1;
-contaCorrenteRenan.depositar(500);
+const contaCorrenteRenan = new ContaCorrente(1001, cliente1);
 
-const conta2 = new ContaCorrente()
-conta2.cliente = cliente1;
-conta2.agencia = 102;
+const conta2 = new ContaCorrente(102, cliente2)
 
 let valor = 200;
 contaCorrenteRenan.transferir(valor, conta2);
 
-conta2.saldo = 3;
-console.log(conta2.saldo);
